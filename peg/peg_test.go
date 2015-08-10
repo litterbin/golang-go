ae32c8caac4c984765ffbe9f8b7526d5f0fb6950
msshin <anarcher@gmail.com>
2015-08-10 14:24:59 +0900

    peg,toki

---
+++ peg/peg_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/pointlander/peg"
+	"testing"
+)
+
+func TestPEGSimple(t *testing.T) {
+
+}
---
+++ toki/toki_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/taylorchu/toki"
+	"testing"
+)
+
+func TestToki(t *testing.T) {
+}
